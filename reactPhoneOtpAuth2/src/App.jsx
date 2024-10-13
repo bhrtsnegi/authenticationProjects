edae67b384208be5ac8c6dd9680f385190cc7596
bhrtsnegi <1bhrtsnegi@gmail.com>
2024-10-13 13:22:40 +0530

    create- basic setup and requirement

---
+++ reactPhoneOtpAuth2/src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <section className="bg-emerald-500 flex items-center justify-center h-screen">
+      <div className="w-80 flex flex-col gap-4 rounded-lg p-4">
+        <h1 className="text-center leading-normal text-white font-medium text-3xl mb-6">
+          Welcome to <br />
+          Log Us In
+        </h1>
+      </div>
+    </section>
+  )
+}
+
+export default App
