eaf9c408916191badc7bfedccfe3cdb8376522b2
bhrtsnegi <1bhrtsnegi@gmail.com>
2024-11-13 12:06:09 +0530

    set and fetch cookies

--- practicejwt/app.js
+++ practicejwt/app.js
@@ -1,10 +1,16 @@
+const cookieParser = require('cookie-parser');
 const express = require('express')
 
 const app = express();
 
+app.use(cookieParser());
 app.get("/", (req, res) =>{
     res.cookie("name", "harsh");
     res.send("done");
 })
 
+app.get("/read", (req, res) =>{
+    console.log(req.cookies);
+    res.send("done");
+})
 app.listen(3000);
