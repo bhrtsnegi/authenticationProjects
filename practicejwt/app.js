2c99a2769af58111934b1b905af0222856c0d753
bhrtsnegi <1bhrtsnegi@gmail.com>
2024-11-13 12:14:48 +0530

    update bcrypt encryption

--- practicejwt/app.js
+++ practicejwt/app.js
@@ -1,12 +1,17 @@
 const cookieParser = require('cookie-parser');
 const express = require('express')
+const bcrypt = require('bcrypt')
 
 const app = express();
 
 app.use(cookieParser());
 app.get("/", (req, res) =>{
-    res.cookie("name", "harsh");
-    res.send("done");
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash("bhimkishakti", salt, function(err, hash) {
+            // Store hash in your password DB.
+            console.log(hash);
+        });
+    });
 })
 
 app.get("/read", (req, res) =>{
