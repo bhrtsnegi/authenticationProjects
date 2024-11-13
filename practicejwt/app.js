af180018e762d18604bfbbdd606503794d4414db
bhrtsnegi <1bhrtsnegi@gmail.com>
2024-11-13 16:58:05 +0530

    worked on jwt sign token passing

--- practicejwt/app.js
+++ practicejwt/app.js
@@ -1,11 +1,16 @@
 const cookieParser = require('cookie-parser');
 const express = require('express')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken');
 
 const app = express();
 
 app.use(cookieParser());
 app.get("/", (req, res) =>{
+    let token = jwt.sign({email: "gaurav@gmail.com"}, "secret");
+    console.log(token);
+    res.cookie("token", token)
+    res.send("Completed")
     bcrypt.genSalt(10, function(err, salt) {
         bcrypt.hash("bhimkishakti", salt, function(err, hash) {
             // Store hash in your password DB.
@@ -15,7 +20,7 @@ app.get("/", (req, res) =>{
 })
 
 app.get("/read", (req, res) =>{
-    console.log(req.cookies);
-    res.send("done");
+    let data = jwt.verify(req.cookies.token, "secret")
+    console.log(data);
 })
 app.listen(3000);
