68919f47a3d5807a1c93148340f08f1fc1e4d050
bhrtsnegi <1bhrtsnegi@gmail.com>
2024-11-15 10:31:15 +0530

    create basic connection setup

---
+++ project1/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const userModel = require('./models/user');
+const cookieParser = require('cookie-parser');
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.send('Hey');
+})
+
+app.post('/create', (req, res) =>{
+    app.render('create')
+})
+app.listen(3000);
